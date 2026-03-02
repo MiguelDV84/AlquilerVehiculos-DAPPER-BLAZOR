a99c8111bb0a84cc0aedbd19dcd7300fd9da102d
Miguel Diaz <miguel.diazv84@gmail.com>
2026-03-02 16:35:22 +0100

    Añadir enums EstadoVehiculo, Role y TipoVehiculo
    Se crean los archivos de enums en WebApiNet.Core.Enums para definir EstadoVehiculo, Role y TipoVehiculo. Se actualiza VehiculoUpdateRequest para usar TipoVehiculo y se corrige su estructura. Se añade el namespace de enums en _Imports.razor y se elimina la definición duplicada de TipoVehiculo.

---
+++ AlquilerVehiculosWeb/AlquilerVehiculosWeb/Enums/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiNet.Core.Enums
+{
+    public enum Role
+    {
+        Admin,
+        User
+    }
+}
--- WebApiNet/AlquilerVehiculosWeb.Web/VehiculoUpdateRequest.cs
+++ WebApiNet/AlquilerVehiculosWeb.Web/VehiculoUpdateRequest.cs
@@ -1,29 +1,14 @@
-﻿
+using WebApiNet.Core.Enums;
 
 namespace WebApiNet.Dto
 {
     public class VehiculoUpdateRequest
     {
         public TipoVehiculo? TipoVehiculo { get; set; }
-
         public int? Kilometraje { get; set; }
-
         public string? Marca { get; set; }
-
         public string? Modelo { get; set; }
-
         public decimal? Precio { get; set; }
-
         public double? LitrosTanque { get; set; }
     }
-}
-
-public enum TipoVehiculo
-{
-    Furgoneta,
-    Camion,
-    Turismo,
-    Deportivo,
-    Monovolumen,
-    Caravana,
 }
