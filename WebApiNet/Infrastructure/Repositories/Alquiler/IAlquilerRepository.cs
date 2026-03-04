551774ddfd2b1d01117927c137170fa8dd292f9f
Miguel Diaz <miguel.diazv84@gmail.com>
2026-03-04 09:44:48 +0100

    Implementación repositorio Alquiler y mejoras paginación
    Se añade AlquilerRepository con Dapper y procedimientos almacenados para CRUD y paginación. Se crea la interfaz IAlquilerRepository. Se mejora la legibilidad de la paginación en AuthRepository usando using var y separando la lectura de total y datos.

---
+++ WebApiNet/Infrastructure/Repositories/Alquiler/IAlquilerRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using WebApiNet.Core.Entities;
+using WebApiNet.Infrastructure.Repositories.Base;
+using WebApiNet.Presentation.Paged;
+
+namespace WebApiNet.Infrastructure.Repositories.AlquilerRepo
+{
+    public interface IAlquilerRepository : IRepository<Alquiler, int>
+    {
+        Task<PagedResult<Alquiler>> GetPagedAsync(int pageNumber, int pageSize);
+    }
+}
