4a3bf618025c8089a4cda749fa730b9edd754db8
Miguel Diaz <miguel.diazv84@gmail.com>
2026-03-13 13:26:11 +0100

    Cambio de relación en LineaDetalle a Alquiler
    Se elimina la propiedad ProductoId y se añade AlquilerId en la clase LineaDetalle, modificando la relación de la línea de detalle para vincularla con un alquiler en lugar de un producto.

--- WebApiNet/Core/Entities/LineaDetalle.cs
+++ WebApiNet/Core/Entities/LineaDetalle.cs
@@ -6,7 +6,7 @@
         public int Cantidad { get; set; }
         public decimal PrecioUnitario { get; set; }
         public decimal Subtotal { get; set; }
-        public int ProductoId { get; set; }
+        public int AlquilerId { get; set; }
         public Alquiler Alquiler { get; set; }
         public int FacturaId { get; set; }
         public Factura Factura { get; set; }
